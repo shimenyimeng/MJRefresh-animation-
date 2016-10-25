7633301bea90368f43a808211c7886c4ff4e0f65
mac <mac@macdemacbook-pro.local>
2016-10-25 17:19:44 +0800

    Initial Commit

---
+++ 018-- MJRefresh(animation)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  018-- MJRefresh(animation)
+//
+//  Created by mac on 16/10/25.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 018-- MJRefresh(animation)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  018-- MJRefresh(animation)
+//
+//  Created by mac on 16/10/25.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
