7998699232c624f920412903161c2feb81a00fcd
shimenyimeng <xuefeigu@163.com>
2016-11-03 15:41:20 +0800

    使用MJRefresh实现下拉刷新动画
    MJRefresh实现下拉刷新动画，其实就是一组图片，包含空闲图片、下拉时的图片以及正在刷新时的图片。只需要传入这三组图片即可

--- 018-- MJRefresh(animation)/ViewController.m
+++ 018-- MJRefresh(animation)/ViewController.m
@@ -7,16 +7,109 @@
 //
 
 #import "ViewController.h"
+#import <MJRefresh.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+    
+    @end
 
-@end
-
-@implementation ViewController
-
+NSString *cellIdentifier = @"cellIdentifier";
+@implementation ViewController {
+    
+    NSMutableArray *_dataListArr;
+    UITableView *_tableView;
+}
+    
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor yellowColor];
+    
+    _tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStylePlain];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
+    [self.view addSubview:_tableView];
+    
+    _dataListArr = [NSMutableArray array];
+    for (NSInteger i = 0; i < 10; i++) {
+        
+        NSString *str = [NSString stringWithFormat:@"%zd", arc4random_uniform(100)];
+        [_dataListArr addObject:str];
+        
+    }
+    
+    MJRefreshGifHeader *header = [MJRefreshGifHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadNewData)];
+    
+    NSMutableArray *idleImages = [NSMutableArray array];
+    for (int i = 1; i<= 40; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"gun%03zd", i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        [idleImages addObject:image];
+    }
+    
+    [header setImages:idleImages forState:MJRefreshStateIdle];
+    
+    NSMutableArray *pullingImages = [NSMutableArray array];
+    for (int i = 1; i<= 67; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"showLove%03zd", i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        [pullingImages addObject:image];
+    }
+    [header setImages:pullingImages forState:MJRefreshStatePulling];
+    
+    
+    NSMutableArray *refreshingImages = [NSMutableArray array];
+    for (int i = 1; i<= 40; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"water%03zd", i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        [refreshingImages addObject:image];
+    }
+    [header setImages:refreshingImages forState:MJRefreshStateRefreshing];
+    
+    // 隐藏刷新时间
+    header.lastUpdatedTimeLabel.hidden = YES;
+    
+    _tableView.mj_header = header;
+    
+}
+
+- (void)loadNewData {
+    
+    for (NSInteger i = 0; i < 10; i++) {
+        
+        NSString *str = [NSString stringWithFormat:@"%zd", arc4random_uniform(100)];
+        [_dataListArr addObject:str];
+        
+        [_tableView reloadData];
+        
+        [_tableView.mj_header endRefreshing];
+    }
+    
+}
+    
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+    
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return _dataListArr.count;
+}
+    
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell == nil) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = _dataListArr[indexPath.row];
+    
+    return cell;
 }
 
 
---
+++ Pods/Target Support Files/Pods-018-- MJRefresh(animation)UITests/Pods-018-- MJRefresh(animation)UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_018___MJRefresh_animation_UITests : NSObject
+@end
+@implementation PodsDummy_Pods_018___MJRefresh_animation_UITests
+@end
